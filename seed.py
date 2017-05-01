ffa52886964f3592edd23d2571ef2963e30da71f
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-01 17:36:17 -0600

    remove all binary fields

--- seed.py
+++ seed.py
@@ -9,5 +9,8 @@ property1 = Property('1062 Delaware St, Denver, CO 80204', 72, db.func.now(),
                      'https://d2tovwv1y8kfyq.cloudfront.net/wp-content/uploads/2015/02/18201559/1062-Delaware-Street-tif-files-002.jpg'
                      )
 
+
+#   '\xe7\xe9' add this to property
+
 db.session.add(property1)
 db.session.commit()
