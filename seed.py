4e61d474ac35a9bcaea173177a62b48bb21b3a59
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-01 11:24:33 -0600

    run seed

---
+++ seed.py
@@ -0,0 +1,12 @@
+from app import db, models
+from models import Property
+
+
+db.session.query(models.Property).delete()
+
+property1 = Property('1062 Delaware St, Denver, CO 80204', 72, db.func.now(),
+                     'https://d2tovwv1y8kfyq.cloudfront.net/wp-content/uploads/2015/02/18201559/1062-Delaware-Street-tif-files-002.jpg')
+
+
+db.session.add(property1)
+db.session.commit()
