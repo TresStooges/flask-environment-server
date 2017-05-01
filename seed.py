9f444790e3f3d9e1181163ff10112c00e21f6b14
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-01 17:32:44 -0600

    take binary out of seed

--- seed.py
+++ seed.py
@@ -6,8 +6,7 @@ db.session.query(models.Property).delete()
 db.session.commit()
 
 property1 = Property('1062 Delaware St, Denver, CO 80204', 72, db.func.now(),
-                     'https://d2tovwv1y8kfyq.cloudfront.net/wp-content/uploads/2015/02/18201559/1062-Delaware-Street-tif-files-002.jpg',
-                     '\xe7\xe9'
+                     'https://d2tovwv1y8kfyq.cloudfront.net/wp-content/uploads/2015/02/18201559/1062-Delaware-Street-tif-files-002.jpg'
                      )
 
 db.session.add(property1)
