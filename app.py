d477a43937b3cedd1c7e50f1bcf304a769a15dd9
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-03 09:18:57 -0600

    back to image url

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 import json
 import os
+import Flask-Uploads
+
 
 
 print('app.py working')
@@ -17,8 +19,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # engine = create_engine('postgres://localhost:5432/envirorpi_db')
 
+
+photos = UploadSet('photos', default_dest=lambda app: app.fileuploadfolder)
+
+
 port = int(os.environ.get('PORT', 5000))
 import models
+
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -69,36 +76,55 @@ def lighting():
     return "Lighting Data"
 
 
-@app.route('/images', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'GET':
-        return 'HASFDASF'
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            print('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            print('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return 'uploaded?'
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+# @app.route('/images', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'GET':
+#         return 'HASFDASF'
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             print('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # if user does not select file, browser also
+#         # submit a empty part without filename
+#         if file.filename == '':
+#             print('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             return 'uploaded?'
+#     return '''
+#     <!doctype html>
+#     <title>Upload new File</title>
+#     <h1>Upload new File</h1>
+#     <form method=post enctype=multipart/form-data>
+#       <p><input type=file name=file>
+#          <input type=submit value=Upload>
+#     </form>
+#     '''
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST' and 'photo' in request.files:
+#         filename = photos.save(request.files['photo'])
+#         rec = Photo(filename=filename, user=g.user.id)
+#         rec.store()
+#         print("Photo saved.")
+#         return redirect(url_for('show', id=rec.id))
+#     return 'GFY'
 
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=port)
+# @app.route('/photo/<id>')
+# def show(id):
+#     photo = Photo.load(id)
+#     if photo is None:
+#         abort(404)
+#     url = photos.url(photo.filename)
+#     return render('gsadgdsgasg')
+
+
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run(host='0.0.0.0', port=port)
