12ec31172ee7a87ebac0daf975b4019509a8fb97
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-04 09:46:45 -0600

    add cors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 import json
@@ -15,6 +16,7 @@ cloudinary.config(
 
 print('app.py working')
 app = Flask(__name__)
+CORS(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/enviropi_db'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
